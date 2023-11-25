15cc13a85c1a9ca475c6417940d974a608a2481a
Dikshit Dhar <79019465+dikshitdhar@users.noreply.github.com>
2023-11-25 20:59:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 
-
 let buttons = document.querySelectorAll('button');
 let input = document.getElementById('inputBox');
 let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const buttonText = e.target.innerHTML;
+        performCalculation(buttonText);
+    });
+});
+
+input.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        performCalculation(input.value);
+    }
+});
+
+function performCalculation(value) {
+    if (value === '=') {
+        try {
             string = eval(string);
             input.value = string;
+        } catch (error) {
+            input.value = 'Error';
+            string = '';
         }
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-        }   
-    })
-})
+    } else if (value === 'AC') {
+        string = '';
+        input.value = string;
+    } else if (value === 'DEL') {
+        string = string.substring(0, string.length - 1);
+        input.value = string;
+    } else {
+        string += value;
+        input.value = string;
+    }
