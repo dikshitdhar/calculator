378b07850ca7797246459b9347a1bf986731060e
Dikshit Dhar <79019465+dikshitdhar@users.noreply.github.com>
2023-11-25 22:11:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
 let buttons = document.querySelectorAll('button');
 let input = document.getElementById('inputBox');
 let string = "";
-
-buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        const buttonText = e.target.innerHTML;
-        performCalculation(buttonText);
-    });
-});
-
-input.addEventListener('input', (e) => {
-    let currentValue = e.target.value;
-    let lastChar = currentValue[currentValue.length - 1];
-
-    if (!(/[0-9]/).test(lastChar)) {
-        e.target.value = currentValue.slice(0, -1);
-    }
-});
-
-function performCalculation(value) {
-    if (value === '=') {
-        try {
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
             string = eval(string);
             input.value = string;
-        } catch (error) {
-            input.value = 'Error';
-            string = '';
         }
-    } else if (value === 'AC') {
-        string = '';
-        input.value = string;
-    } else if (value === 'DEL') {
-        string = string.substring(0, string.length - 1);
-        input.value = string;
-    } else {
-        string += value;
-        input.value = string;
-    }
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }   
+    })
+})
